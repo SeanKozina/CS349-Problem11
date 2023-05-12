e7f9b347a251a2c728466da35546e0c96d041cb6
SeanKozina <120601630+SeanKozina@users.noreply.github.com>
2023-05-12 11:29:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,86 +7,90 @@ using namespace std;
 
 class Grid{
 
-    public:
-        vector<vector<bool>> grid;
+public:
+    vector<vector<bool>> grid;
 
-        int grid_h(){
-            return grid.size();
+    int grid_h(){
+        return grid.size();
+    }
+
+    int grid_w(){
+        return grid[0].size();
+    }
+
+    void set_row(int row_coord, bool value){
+        for (int i = 0; i < grid[row_coord].size(); i++){
+            grid[row_coord][i] = value;
+        }
+    }
+
+    void set_grid(bool value){
+        for (int i = 0; i < grid.size(); i++){
+            set_row(i, value);
+        }
+    }
+
+    void create_grid(int radius){
+        int grid_width = (2*radius) + 1;
+        int grid_height = (2*radius) + 1;
+
+        vector<vector<bool>> newGrid;
+        for (int i = 0; i < grid_height; i++){
+            // create a new row
+            vector<bool> newRow;
+            for (int k = 0; k < grid_width; k++){
+                newRow.push_back(false);
+            }
+            // push row
+            newGrid.push_back(newRow);
         }
 
-        int grid_w(){
-            return grid[0].size();
+        grid = newGrid;
+    }
+
+
+    void print_grid(){
+        for (int i = 0; i < grid_h(); i++){
+            // create a new row
+            vector<bool> newRow;
+            for (int k = 0; k < grid_w(); k++){
+                cout << "■ ";
+            }
+            // push row
+            cout << "\n";
         }
-
-        void set_row(int row_coord, bool value){
-            for (int i = 0; i < grid[row_coord].size(); i++){
-                grid[row_coord][i] = value;
-            }
-        } 
-
-        void set_grid(bool value){
-            for (int i = 0; i < grid.size(); i++){
-                set_row(i, value);
-            }
-        } 
-
-        void create_grid(int radius){
-            int grid_width = (2*radius) + 1;
-            int grid_height = (2*radius) + 1;
-            
-            vector<vector<bool>> newGrid;
-            for (int i = 0; i < grid_height; i++){
-                // create a new row
-                vector<bool> newRow;
-                for (int k = 0; k < grid_width; k++){
-                    newRow.push_back(false);
-                }
-                // push row
-                newGrid.push_back(newRow);
-            }
-
-            grid = newGrid;
-        }
-
-
-        void print_grid(){
-            for (int i = 0; i < grid_h(); i++){
-                // create a new row
-                vector<bool> newRow;
-                for (int k = 0; k < grid_w(); k++){
-                    cout << "■ ";
-                }
-                // push row
-                cout << "\n";
-            }
-        }
+    }
 
 };
+
+
+
 
 class Ant{
-    
-    public:
 
-        int x, y;
-        // can be "up" , "down" , "left" , "right"
-        string direction;
+public:
 
-        Ant(){
-            x = 0;
-            y = 0;
-            direction = "up";
-        }
+    int x, y;
+    // can be "up" , "down" , "left" , "right"
+    string direction;
 
-        void print_ant(){
-            cout << "Ant Information:\n";
-            cout << "   x: " << x << endl << "   y: " << y << endl;
-            cout << "Facing Direction: " << direction << endl;
-        }
+    Ant(){
+        x = 0;
+        y = 0;
+        direction = "up";
+    }
 
+    void print_ant(){
+        cout << "Ant Information:\n";
+        cout << "   x: " << x << endl << "   y: " << y << endl;
+        cout << "Facing Direction: " << direction << endl;
+    }
+
+    void processAnt(Grid& grid){
+        x = grid.grid_h();
+        y = x;
+    }
 };
-
-
-
 
 
 
@@ -129,9 +133,8 @@ int main(int argc, char* argv[]) {
 
     Ant a;
     a.print_ant();
-
-
-
+    a.processAnt(g);
+    a.print_ant();
 
     return 0;
 }
