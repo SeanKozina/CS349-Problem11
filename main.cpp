a32732595cdd3afbb931f8a416f0b31a0a7c7020
Doctor Laplace <58324905+DoctorLaplace@users.noreply.github.com>
2023-05-12 10:36:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,62 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
 #include <string>
-
+#include <vector>
+#include <fstream>
 using namespace std;
 
 
+class grid{
+
+public:
+    vector<bool>
+
+
+};
+
+
+
+
+
+
+
+
+
 int main(int argc, char* argv[]) {
+    // Check for correct number of arguments
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " input_file output_file" << std::endl;
+        cout << "Usage: " << argv[0] << " <input_file> <output_file>\n";
         return 1;
     }
 
-    std::string input_file_name = argv[1];
-    std::string output_file_name = argv[2];
-
-    std::ifstream input_file("/Users/student/CLionProjects/Chess/inputfile.txt");
-    if (!input_file) {
-        std::cerr << "Error: could not open input file '" << input_file_name << "'" << std::endl;
+    // Open input file
+    ifstream input_file(argv[1]);
+    if (!input_file.is_open()) {
+        cout << "Unable to open input file.\n";
         return 1;
     }
-    
 
-    /*
-    std::string move_str;
-    while (input_file >> move_str) {
-
-
-        }
+    // Open output file
+    ofstream output_file(argv[2]);
+    if (!output_file.is_open()) {
+        cout << "Unable to open output file.\n";
+        return 1;
     }
-    */
+
+
+
+
+
+
+
+
 
 
     return 0;
 }
+
+
+
+
+
+
 
